16ac3e0906da297df05d546c6ee6c024ac239aa7
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-07-09 20:41:47 +0200

    update CssVanilla component

--- src/pages/CssVanilla.jsx
+++ src/pages/CssVanilla.jsx
@@ -1,11 +1,19 @@
+import { Link } from 'react-router-dom';
 import '../styles/CssVanilla.scss';
 
 export const CssVanilla = () => {
    return (
       <>
+         <nav className="nav-css">
+            <ul>
+               <Link to="/">
+                  <li>Home</li>
+               </Link>
+            </ul>
+         </nav>
          <section className="bg-1">
             <div>
-               <h1>Selva Amazónica 1</h1>
+               <h1 className="title">Selva Amazónica 1</h1>
             </div>
          </section>
          <div className="content">
@@ -14,7 +22,7 @@ export const CssVanilla = () => {
             ducimus maiores sequi quo temporibus unde consectetur facere vero, architecto dolore eos.
          </div>
          <section className="bg-2">
-            <h1>Selva Amazónica 3</h1>
+            <h1 className="title">Selva Amazónica 2</h1>
          </section>
          <div className="content">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam corrupti quod a possimus corporis
@@ -22,7 +30,7 @@ export const CssVanilla = () => {
             ducimus maiores sequi quo temporibus unde consectetur facere vero, architecto dolore eos.
          </div>
          <section className="bg-3">
-            <h1>Section 3</h1>
+            <h1 className="title">Selva Amazónica 3</h1>
          </section>
          <div className="content">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam corrupti quod a possimus corporis
